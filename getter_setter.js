5e6a947eee17257f20ab523de24910cf0a55f97e
amrendra1998 <techupdate855@gmail.com>
2025-05-30 23:18:01 +0530

    file committed

--- getter_setter.js
+++ getter_setter.js
@@ -1,24 +1,53 @@
-class User {
-    constructor(email, password){
-        this.email = email;
-        this.password = password
+// class User {
+//     constructor(email, password){
+//         this.email = email;
+//         this.password = password
+//     }
+
+//     get email(){
+//         return this._email.toUpperCase()
+//     }
+//     set email(value){
+//         this._email = value
+//     }
+
+//     get password(){
+//         return `${this._password}hitesh`
+//     }
+
+//     set password(value){
+//         this._password = value
+//     }
+// }
+
+// const hitesh = new User("h@hitesh.ai", "abc")
+// console.log(hitesh.email);
+
+
+
+class Person {
+    constructor(firstname,lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+    }
+    get fullname(){
+        return `${this.firstname} ${this.lastname}`
     }
 
-    get email(){
-        return this._email.toUpperCase()
-    }
-    set email(value){
-        this._email = value
-    }
-
-    get password(){
-        return `${this._password}hitesh`
-    }
-
-    set password(value){
-        this._password = value
+    set fullname(name){
+         const [firstname, lastname] = name.split(" ");
+         this.firstname = firstname;
+         this.lastname = lastname;
     }
 }
 
-const hitesh = new User("h@hitesh.ai", "abc")
-console.log(hitesh.email);
+const person = new Person("Amrendra","kumar")
+console.log(person.fullname);
+
+person.fullname = "Dragn kumar";
+console.log(person.firstname);
+console.log(person.lastname);
+
+
+
